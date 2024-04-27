ee464db78639c021543d88f8093410ccc418399d
KIRAN NEGI <163193047+KiranNegii@users.noreply.github.com>
2024-04-27 13:56:20 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+import styles from "./App.module.css";
+import { useState } from "react";
+
+function App() {
+  const [calVal, setCalVal] = useState("");
+
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
+
+  return (
+    <>
+    <h4>CALCULATOR</h4>
+    <div className={styles.calculator}>
+      <Display displayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+    </div>
+    </>
+  );
+}
+
+export default App;
